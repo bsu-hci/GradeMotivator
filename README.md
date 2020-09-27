fdc33eac484f86439e2b7862ced5542fb1a42e3c
Austin Foster <afoster@bsu.edu>
2020-09-26 21:41:19 -0400

    Bulk code  added

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
 # GradeMotivator
 
-A new Flutter project.
+Use this application to calculate a grade on a test!
+It will provide you with feedback and motivation to keep studying.
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+NOTE | If your grade is a percentage and not a fraction, Enter 100 for the total points and it will be accurate.
--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,157 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(GradeMotivator());
+void main() => runApp(GradeMotivator());
+
+class Grade {
+  int _userScore;
+  int _totalPossiblePoints;
+  bool _show;
+  String _grade;
+
+  Grade() {
+    _grade = "";
+    _show = false;
+  }
+}
+
+class Show extends StatelessWidget {
+  final String _grade;
+  Show(this._grade);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(children: [
+      Text(
+        "You recieved a(n)",
+        textAlign: TextAlign.center,
+      ),
+      Text(
+        _grade,
+        textScaleFactor: 2,
+        textAlign: TextAlign.center,
+      )
+    ]);
+  }
 }
 
 class GradeMotivator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Grade Motivator',
+      title: "Welcome to Grade Motivator",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+        primarySwatch: Colors.grey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Welcome to Grade Motivator'),
+      home: Scaffold(
+        appBar: AppBar(title: Text("Motivation Starts Here")),
+        body: Center(
+            child: Container(
+          constraints: BoxConstraints(maxWidth: 350),
+          padding: EdgeInsets.all(30),
+          child: GenerateGrade(),
+        )),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class GenerateGrade extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => _GenerateGradeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GenerateGradeState extends State<GenerateGrade> {
+  final _fk = GlobalKey<FormState>();
+  var _numberGradeInput;
+  var _totalPosibleInput;
+  Grade grade = new Grade();
 
-  void _incrementCounter() {
+  void _onCalculateButtonPressed() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      this.grade._show = true;
+      this.grade._grade = calculateGrade(
+          this.grade._userScore, this.grade._totalPossiblePoints);
     });
+  }
+
+  String calculateGrade(int myScore, int totalPossiblePoints) {
+    double percentage = (myScore / totalPossiblePoints) * 100;
+    if (percentage >= 100)
+      return "A+ : Outstanding work! Extra credit pays off!";
+    if (percentage >= 90)
+      return "A : Great Job! Keep It up! Aim even higher next time!";
+    if (percentage >= 80)
+      return "B : Not Too Bad! One step below perfection! Keep going!";
+    if (percentage >= 70)
+      return "C : Still Passing, You can do better! Try asking for some extra help.";
+    if (percentage >= 60)
+      return "D : Maybe try making some notecards! Better study habits are a must.";
+    if (percentage < 60)
+      return "F : Spend more time hittin' the books! You will get it next time.";
+    return "Invalid";
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    return Form(
+        key: _fk,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            TextFormField(
+              textAlign: TextAlign.right,
+              decoration: const InputDecoration(hintText: "My Points"),
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly,
+              ],
+              keyboardType: TextInputType.number,
+              onSaved: (newValue) {
+                this._numberGradeInput = int.parse(newValue);
+              },
+              validator: (value) {
+                if (value.isEmpty) {
+                  return "Must enter grade %";
+                }
+                return null;
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            TextFormField(
+              textAlign: TextAlign.right,
+              decoration:
+                  const InputDecoration(hintText: "Total Possible Points"),
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly,
+              ],
+              keyboardType: TextInputType.number,
+              onSaved: (newNumber) {
+                this._totalPosibleInput = int.parse(newNumber);
+              },
+              validator: (number) {
+                if (number.isEmpty) {
+                  return "Must enter a total";
+                }
+                return null;
+              },
             ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5.0),
+              child: RaisedButton(
+                onPressed: () {
+                  if (_fk.currentState.validate()) {
+                    _fk.currentState.save();
+                    this.grade._userScore = this._numberGradeInput;
+                    this.grade._totalPossiblePoints = this._totalPosibleInput;
+                    _onCalculateButtonPressed();
+                  }
+                },
+                child: Text('Calculate Grade'),
+              ),
+            ),
+            Flexible(
+                child: this.grade._show ? Show(this.grade._grade) : Text(""))
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        ));
   }
 }
